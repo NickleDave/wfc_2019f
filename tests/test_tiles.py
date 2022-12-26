dab4dfa59f7f76ec691f6593a3bee48806e4b01d
David Nicholson <nickledave@users.noreply.github.com>
2022-12-25 23:01:19 -0500

    Use absolute reference, v2.imread in test_tiles.py

--- tests/test_tiles.py
+++ tests/test_tiles.py
@@ -3,22 +3,22 @@ from __future__ import annotations
 
 import imageio  # type: ignore
 from tests.conftest import Resources
-from wfc import tiles
+import wfc.tiles
 
 
 def test_image_to_tile(resources: Resources) -> None:
     filename = resources.get_image("samples/Red Maze.png")
-    img = imageio.imread(filename)
-    tiles = tiles.image_to_tiles(img, 1)
+    img = imageio.v2.imread(filename)
+    tiles = wfc.tiles.image_to_tiles(img, 1)
     assert tiles[2][2][0][0][0] == 255
     assert tiles[2][2][0][0][1] == 0
 
 
 def test_make_tile_catalog(resources: Resources) -> None:
     filename = resources.get_image("samples/Red Maze.png")
-    img = imageio.imread(filename)
+    img = imageio.v2.imread(filename)
     print(img)
-    tc, tg, cl, ut = tiles.make_tile_catalog(img, 1)
+    tc, tg, cl, ut = wfc.tiles.make_tile_catalog(img, 1)
     print("tile catalog")
     print(tc)
     print("tile grid")
